8540e0abe11061b87b5248322782c34ba546aaec
Ben <robinsonbena@gmail.com>
2022-06-21 13:44:23 -0700

    some pseudocode

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,15 @@
 class StaticPagesController < ApplicationController
   
   def home
+    return unless params[:search].present?
+
+    Flick.new #API key, #Shared secret
+
   end
-  
+
+  # if params are not blank
+  #   send request(params) to flickr API
+  #   receive text file
+  #   create urls for each record in text file - display in the _images partial
+
 end
