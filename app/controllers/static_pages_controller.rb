9676350ff5eae4cc5212c1cf352fef6cfecdd549
Ben <robinsonbena@gmail.com>
2022-06-22 11:39:48 -0700

    refactor #home in static_pages controller and amend conditional in home view to render failed search result if no images match the entered tag

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,13 +4,7 @@ class StaticPagesController < ApplicationController
     return unless params[:search].present?
 
     flickr = Flickr.new
-    @json_output = flickr.photos.search tags: params[:search]
-    @links = @json_output.map { |i| Flickr.url_m(i) }
+    @links = flickr.photos.search(tags: params[:search]).map { |i| Flickr.url_m(i) }
   end
-
-  # if params are not blank
-  #   send request(params) to flickr API
-  #   receive text file
-  #   create urls for each record in text file - display in the _images partial
 
 end
